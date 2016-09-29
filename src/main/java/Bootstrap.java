f53009dc5bae6e759f409be958f894ed4dda4e17
Andrey.Gabchak <andrey.gabchak@gmail.com>
2016-09-29 23:03:11 +0300

    Added the choice (date or equation) for calculation

--- src/main/java/Bootstrap.java
+++ src/main/java/Bootstrap.java
@@ -1,8 +1,10 @@
+import DateOperations.DateParserAndCalculate;
 import Operations.AllOperations;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Bootstrap {
 
@@ -17,14 +19,27 @@ public class Bootstrap {
         allOperations.addBinaryOperation(new Division());
         allOperations.addBinaryOperation(new Multiplication());
 
+        System.out.println("Choose what needs calculate.\nFor Date enter 1\nFor equation enter 2");
         Scanner scanner = new Scanner(System.in);
-        String inputExpression = scanner.next();
+        String choice = scanner.next();
 
-        Arithmetic arithmetic = new Arithmetic();
+        String inputExpression;
+        if (choice.equals("1")) {
+            System.out.println("The date format is 2016.09.15-2016.09.14");
+            inputExpression = scanner.next();
+            DateParserAndCalculate dateParser = new DateParserAndCalculate();
+            dateParser.parseDate(inputExpression);
+            dateParser.calculateDate();
+        } else if (choice.equals("2")) {
+            System.out.println("The equation format is 1+2^4*(3+3^3)/3");
+            inputExpression = scanner.next();
+            Parser parser = new Parser();
+            Stack<String> stackPRN = parser.parsing(inputExpression);
 
-        double result = arithmetic.calculation(inputExpression);
-        System.out.println(result);
+            Arithmetic arithmetic = new Arithmetic();
+            double result = arithmetic.calculation(stackPRN);
 
-
+            System.out.println(result);
+        }
     }
 }
