197aca56956acadc2cf9cdeba8f50a85748827ab
andrey-gabchak <andrey.gabchak@gmail.com>
2016-10-11 23:23:33 +0300

    The type must be 'ProceedingJoinPoint' not 'MethodInvocationProceedingJoinPoint'

--- src/main/java/LogAspect.java
+++ src/main/java/LogAspect.java
@@ -1,9 +1,9 @@
-import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 public class LogAspect {
 
-    public Object logging(MethodInvocationProceedingJoinPoint pjp) throws Throwable {
-        System.out.println("Start " + pjp.getSignature().getName() + " method ");
+    public Object logging(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Start " + pjp.getSignature().getName() + " method");
         Object result = pjp.proceed();
         System.out.println("Finish " + pjp.getSignature().getName() + " method");
         return result;
